07703a80b979dd8c311639ee18165613165f9e4c
Eddie Harris <harrisejm@gmail.com>
2018-09-24 14:30:59 -0700

    update board

--- testCode/testChess.js
+++ testCode/testChess.js
@@ -14,19 +14,38 @@ let BoardPos = function(positionY, positionX, occupied) {
 let arr = [];   /// this will be STATE
 let arrTest = [];
 
-
+let selectedRow = 0;
 for (let i = 0; i < 8; i++) {
 let positionArr = [];
 let objectArr = [];
+selectedRow += 1;
+
+
   for (let a = 0; a < 8; a++) {
   //let position = board[i][a];
 // let position = "[" + i + "," + a +"]";
    if (i === 0 || i === 1 || i === 6 || i === 7) {
      let position = new BoardPos(i,a,true);
      objectArr.push(position);
+
+     let space = document.createElement("td");
+     let node = document.createTextNode("[" + i + "," + a +"]" + position.occupied);
+     space.appendChild(node);
+    //  if (a === 0) {
+    //    selectedRow += 1;
+    //  }
+     let element = document.getElementById("row" + selectedRow.toString());
+     element.appendChild(space);
+
    } else {
        let position = new BoardPos(i,a,false);
        objectArr.push(position);
+
+       let space = document.createElement("td");
+       let node = document.createTextNode("[" + i + "," + a +"]" + position.occupied);
+       space.appendChild(node);
+       let element = document.getElementById("row" + selectedRow.toString());
+       element.appendChild(space);
    }
    positionArr.push("[" + i + "," + a +"]");
    if (a === 7) {
@@ -34,8 +53,19 @@ let objectArr = [];
      arrTest.push(objectArr);
      //position.length = 0;
    }
+
   }
+
 }
+///populate board
+// for (let i = 0; i < 8; i++) {
+//   let test = i;
+//   let space = document.createElement("td");
+//   let node = document.createTextNode(test.toString());
+//   space.appendChild(node);
+//   let element = document.getElementById("row1");
+//   element.appendChild(space);
+// }
 
 
 
@@ -47,20 +77,31 @@ let Pawn = function(firstMove, selected, color, taken, positionY, positionX) {
   this.taken = taken;
   this.positionY = positionY;
   this.positionX = positionX;
+  this.movePawn = function(testMove){
+
+    if (testMove === board[this.positionY - 1][this.positionX]) {
+      this.positionX = this.positionX;
+      this.positionY = this.positionY - 1;
+    } else if (this.firstMove === true && testMove === board[this.positionY - 2][this.positionX]) {
+      this.positionX = this.positionX;
+      this.positionY = this.positionY - 2;
+      this.firstMove = false;
+    }
+  }
 }
 
 let testMove = board[1][2];
 
-Pawn.prototype.movePawn = function(testMove) {
-  if (testMove === board[this.positionY - 1][this.positionX]) {
-    this.positionX = this.positionX;
-    this.positionY = this.positionY - 1;
-  } else if (this.firstMove === true && testMove === board[this.positionY - 2][this.positionX]) {
-    this.positionX = this.positionX;
-    this.positionY = this.positionY - 2;
-    this.firstMove = false;
-  }
-}
+// Pawn.prototype.movePawn = function(testMove) {
+//   if (testMove === board[this.positionY - 1][this.positionX]) {
+//     this.positionX = this.positionX;
+//     this.positionY = this.positionY - 1;
+//   } else if (this.firstMove === true && testMove === board[this.positionY - 2][this.positionX]) {
+//     this.positionX = this.positionX;
+//     this.positionY = this.positionY - 2;
+//     this.firstMove = false;
+//   }
+// }
 
 pawn1 = new Pawn(true, true, "white", false, 3, 2);
 pawn1.movePawn(testMove);
@@ -73,9 +114,14 @@ for (let b = 0; b < arrTest.length; b++) {
   }
 }
 //////
+
+
+//let firstVar = "1";
+//document.getElementById(firstVar).innerHTML = "hello";
+
 document.getElementById("test").innerHTML = board[pawn1.positionY][pawn1.positionX];
 document.getElementById("test1").innerHTML = pawn1.positionY;
 document.getElementById("test2").innerHTML = pawn1.positionX;
 document.getElementById("test3").innerHTML = arr;
-document.getElementById("test4").innerHTML = arrTest;
+document.getElementById("test4").innerHTML = arrTest[0][0].occupied;
 document.getElementById("test5").innerHTML = hello;
