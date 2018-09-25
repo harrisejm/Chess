e192aa01da58ae0fff480175c34b7227c8527b5d
Eddie Harris <harrisejm@gmail.com>
2018-09-25 14:14:23 -0700

    update state to hold array of position objects

--- src/components/MainBoard.jsx
+++ src/components/MainBoard.jsx
@@ -10,15 +10,49 @@ class MainBoard extends React.Component {
       board: []
     };
     this.populateBoard = this.populateBoard.bind(this);
-
   }
 
 populateBoard() {
   let newBoard = this.state.board.slice();
-  newBoard.push(updatedPosition);
-  this.setState({board: newBoard});
+  let objectArr = [];
+  //let arr = [];
+  for (let i = 0; i < 8; i++) {
+  //let positionArr = [];
 
+    for (let a = 0; a < 8; a++) {
+      let position = i.toString() + a.toString();
+     if (i === 0 || i === 1 || i === 6 || i === 7) {
+
+     let test1 = Object.assign({positionY: i, positionX: a, occupied: true}, {});
+
+    // objectArr.push( position: { positionY: i, positionX: a, occupied: true });
+     objectArr.push(test1);
+     } else {
+       let test2 = Object.assign({positionY: i, positionX: a, occupied: false}, {});
+       objectArr.push(test2);
+        // { positionY: i, positionX: a, occupied: false }
+    //     objectArr.push( position: { positionY: i, positionX: a, occupied: false });
+     }
+
+     if (a === 7) {
+    //   arr.push(positionArr);
+    //   objectArr.push(" ");
+       newBoard.push(objectArr);
+       objectArr = [];
+   }
+
+
+    }
   }
+//newBoard.push(objectArr);
+//const arr = newBoard.join("").trim().split(" ");
+  //newBoard.push(updatedPosition);
+  this.setState({board: newBoard});
+  console.log(this.state)
+  }
+
+
+
 
 testRender(){
   let test = {
@@ -31,18 +65,21 @@ testRender(){
   return chessBoard;
 }
 
-
 render(){
-
 
   return (
     <div>
      <p>dsgfdghghghfg</p>
-       {this.testRender()}
+
+       <p onClick={this.populateBoard}>Push</p>
     </div>
   );
 }
 }
+
+// App.propTypes = {
+//   populateBoard: PropTypes.func
+// };
 
 
 
