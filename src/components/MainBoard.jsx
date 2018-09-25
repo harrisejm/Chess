ee415f385da82be5bf5e0d84a616dfc801441c13
Eddie Harris <harrisejm@gmail.com>
2018-09-25 16:21:48 -0700

    add

--- src/components/MainBoard.jsx
+++ src/components/MainBoard.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 
+
+
 class MainBoard extends React.Component {
 
   constructor(props) {
@@ -10,58 +12,82 @@ class MainBoard extends React.Component {
       board: []
     };
     this.populateBoard = this.populateBoard.bind(this);
+    this.testRender = this.testRender.bind(this);
+    this.testAdd = this.testAdd.bind(this);
   }
 
 populateBoard() {
   let newBoard = this.state.board.slice();
-  let objectArr = [];
-  //let arr = [];
-  for (let i = 0; i < 8; i++) {
-  //let positionArr = [];
+let objectArr = [];
 
+  for (let i = 0; i < 8; i++) {
     for (let a = 0; a < 8; a++) {
       let position = i.toString() + a.toString();
      if (i === 0 || i === 1 || i === 6 || i === 7) {
 
      let test1 = Object.assign({positionY: i, positionX: a, occupied: true}, {});
 
-    // objectArr.push( position: { positionY: i, positionX: a, occupied: true });
      objectArr.push(test1);
      } else {
        let test2 = Object.assign({positionY: i, positionX: a, occupied: false}, {});
        objectArr.push(test2);
-        // { positionY: i, positionX: a, occupied: false }
-    //     objectArr.push( position: { positionY: i, positionX: a, occupied: false });
      }
-
      if (a === 7) {
     //   arr.push(positionArr);
     //   objectArr.push(" ");
        newBoard.push(objectArr);
        objectArr = [];
    }
-
-
     }
   }
-//newBoard.push(objectArr);
-//const arr = newBoard.join("").trim().split(" ");
-  //newBoard.push(updatedPosition);
   this.setState({board: newBoard});
-  console.log(this.state)
+  console.log(this.state.board.length);
+  if (this.state.board.length === 8) {
+  //  testAdd()
+  }
+//  return this.testRender();
   }
 
+//  const { expect } = window;
 
+  // window.onload = function() {
+  //   this.populateBoard();
+  // }
 
+testAdd() {
+//  return this.state.board[0][0].occupied;
+}
 
 testRender(){
   let test = {
     color: "green"
   }
+
   const chessBoard =
     <div>
       <p style={test}>Does this work</p>
-    </div>;
+    <table>
+      <tbody>
+         <tr className="row1">
+           <td> {this.state.length} TESTTEST</td>
+         </tr>
+         <tr className="row2">
+         </tr>
+         <tr className="row3">
+         </tr>
+         <tr className="row4">
+         </tr>
+         <tr className="row5">
+         </tr>
+         <tr className="row6">
+         </tr>
+         <tr className="row7">
+         </tr>
+         <tr className="row8">
+         </tr>
+      </tbody>
+    </table>
+ </div>;
   return chessBoard;
 }
 
@@ -70,7 +96,7 @@ render(){
   return (
     <div>
      <p>dsgfdghghghfg</p>
-
+       {this.testRender()}
        <p onClick={this.populateBoard}>Push</p>
     </div>
   );
